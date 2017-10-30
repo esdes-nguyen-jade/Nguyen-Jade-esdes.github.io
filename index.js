8b7eb357640376c86ba9b6712e60f9885fc63525
esdes-nguyen-jade <32100497+esdes-nguyen-jade@users.noreply.github.com>
2017-10-30 16:44:27 +0100

    new modifications

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 console.log(document);
 
-var slideIndex = 0;
-carousel();
-
-function carousel() {
-    var i;
-    var x = document.getElementsByClassName("mySlides");
-    for (i = 0; i < x.length; i++) {
-      x[i].style.display = "none"; 
+function myFunction() {
+    var x = document.getElementById("Demo");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else { 
+        x.className = x.className.replace(" w3-show", "");
     }
-    slideIndex++;
-    if (slideIndex > x.length) {slideIndex = 1} 
-    x[slideIndex-1].style.display = "block"; 
-    setTimeout(carousel, 2000); // Change image every 2 seconds
 }
+
+
